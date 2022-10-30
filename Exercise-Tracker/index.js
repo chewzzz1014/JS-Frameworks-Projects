ccf3d4bab0babcd17d85ab3bbd327365533db811
chewzzz <chewziqing@gmail.com>
2022-10-30 19:20:58 +0800

    Add first POST request

--- Exercise-Tracker/index.js
+++ Exercise-Tracker/index.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("./models/user");
-const Exercise = require("./models/exercise");
+const { User } = require("./models/user");
+const { Exercise } = require("./models/exercise");
+const path = require("path");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }))
 // to be able to read .env file
 require("dotenv").config()
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('mongodb://localhost:27017/exercise', { useNewUrlParser: true })
+    .then(() => {
+        console.log("mongo connection open");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render("home");
+    //res.send("ok")
+})
+
+app.post("/api/users", async (req, res, next) => {
+    const { username } = req.body;
+    const user = new User({ username: username });
+    await user.save();
+
+    const { username: fUsername, _id } = await User.findOne({ username: username })
+    res.json({ username: fUsername, _id: _id })
 })
 
 app.listen(3000, () => {
