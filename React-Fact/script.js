f9009c55b28c0b59726ca91f33fdca557d249319
chewzzz <chewziqing@gmail.com>
2022-11-04 17:26:42 +0800

    Add JFX into react component

--- React-Fact/script.js
+++ React-Fact/script.js
@@ -4,21 +4,23 @@ import ReactDOM from 'react-dom';
 // a react component that returns JSX
 // render JSX at root
 // JSC only return a single parent element (others are nested inside the parent)
-function getPage() {
-    <div>
-        <img src='./logo512.png' width='40px' />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div >
+function GetPage() {
+    return (
+        < div >
+            <img src='./logo512.png' width='40px' />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div >
+    )
 }
 
 ReactDOM.render(
-    getPage(),
+    <GetPage />,
     document.querySelector("#root")
 );
