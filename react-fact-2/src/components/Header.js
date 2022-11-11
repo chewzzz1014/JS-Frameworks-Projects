888b9e4cad2f0fd79e54206c71d622ea6bce19c3
chewzzz <chewziqing@gmail.com>
2022-11-11 15:53:52 +0800

    Add style and logo

--- react-fact-2/src/components/Header.js
+++ react-fact-2/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 function Header() {
     return (
         <header>
             <nav className="nav">
-                <img src='./react-logo.png' />
+                <img src={logo} />
 
                 {/* Add style */}
                 <ul className="nav-items">
