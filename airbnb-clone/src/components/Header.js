a1df4ee72673c2281bea205e32b446528fcd0b1a
chewzzz <chewziqing@gmail.com>
2022-11-13 15:56:09 +0800

    Start project and add nav bar

---
+++ airbnb-clone/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaAirbnb } from 'react-icons/fa';
+
+function Header() {
+    return (
+        <nav>
+            <FaAirbnb size='2.5em' color='#FF5A5F' />
+            <h1>airbnb</h1>
+        </nav>
+    )
+}
+
+export default Header;
