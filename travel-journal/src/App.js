8b70b9288281f263af0a2ee0177649537036696d
chewzzz <chewziqing@gmail.com>
2022-11-18 13:59:22 +0800

    Add comment

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Article from './components/Article';
 import data from './data/location'
 
+// create Article component for all locations
 const articleComponent = data.map((ele) => {
   return <Article data={ele} />
 })
