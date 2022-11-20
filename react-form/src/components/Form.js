57ce14eef808e2548b81ff1c795f97c0063fc685
chewzzz <chewziqing@gmail.com>
2022-11-20 18:09:26 +0800

    Add generic onChange handler

--- react-form/src/components/Form.js
+++ react-form/src/components/Form.js
@@ -2,29 +2,44 @@ import React, { useState } from 'react';
 
 export default function Form() {
 
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: ""
+    })
 
-    function handleFirstNameChange(e) {
-        setFirstName(e.target.value)
+    function handleChange(e) {
+
+        setFormData((prev) => {
+            return {
+                ...prev,
+                [e.target.name]: [e.target.value]
+            }
+        })
     }
 
-    function handleLastNameChange(e) {
-        setLastName(e.target.value)
-    }
+    // function handleFirstNameChange(e) {
+    //     setFirstName(e.target.value)
+    // }
+
+    // function handleLastNameChange(e) {
+    //     setLastName(e.target.value)
+    // }
 
     return (
         <form>
             <input
                 type='text'
                 placeholder='First name'
-                onChange={handleFirstNameChange}
+                name='firstName'
+                onChange={handleChange}
             />
             <input
                 type='text'
                 placeholder='Last name'
-                onChange={handleLastNameChange}
+                name='lastName'
+                onChange={handleChange}
             />
+            <p>{`${formData.firstName} ${formData.lastName}`}</p>
         </form>
     )
 } 
