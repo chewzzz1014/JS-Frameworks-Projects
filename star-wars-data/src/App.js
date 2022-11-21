9dec510f4b30bd6659c0661b919c765d106f278b
chewzzz <chewziqing@gmail.com>
2022-11-21 22:10:03 +0800

    Add useEffect

--- star-wars-data/src/App.js
+++ star-wars-data/src/App.js
@@ -5,10 +5,24 @@ function App() {
 
   const apiLink = 'https://swapi.dev/api/people/1/'
   const [starWarsData, setStarWarsData] = useState({})
+  const [count, setCount] = useState(0)
+
+  function btnHandler() {
+    setCount((prev) => prev + 1)
+  }
+
+  useEffect(() => {
+    console.log("effect rendered")
+    fetch(apiLink)
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+  }, [count])
 
   return (
     <div className="App">
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <p>The count is {count}</p>
+      <button onClick={btnHandler}>Add</button>
     </div>
   );
 }
