5d803b0d9b2e63b0a487eca467201db2c1465867
chewzzz <chewziqing@gmail.com>
2022-11-22 17:53:36 +0800

    Generate Number component using array method

--- tenzies-game/src/components/Die.js
+++ tenzies-game/src/components/Die.js
@@ -2,21 +2,26 @@ import React, { useState } from 'react'
 import Number from './Number'
 
 export default function Die() {
+
+    function allNewDice() {
+        const randomNum = () => Math.floor(Math.random() * 6) + 1
+
+        const arr = []
+        for (let i = 0; i < 5; i++)
+            arr.push(randomNum())
+
+        return arr.map((ele) =>
+            <Number value={ele} />
+        )
+    }
+
     return (
         <div className='number-section'>
-            <div className='number-row-1'>
-                <Number value={1} />
-                <Number value={1} />
-                <Number value={1} />
-                <Number value={1} />
-                <Number value={1} />
+            <div className='number-row'>
+                {allNewDice()}
             </div>
-            <div className='number-row-2'>
-                <Number value={1} />
-                <Number value={1} />
-                <Number value={1} />
-                <Number value={1} />
-                <Number value={1} />
+            <div className='number-row'>
+                {allNewDice()}
             </div>
         </div>
     )
