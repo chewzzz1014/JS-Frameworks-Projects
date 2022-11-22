e37f57f204880934671b667580cdca213e2df584
chewzzz <chewziqing@gmail.com>
2022-11-22 17:01:26 +0800

    Add layoyut and styles

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
 import React, { useStare } from 'react'
+import Number from './components/Number';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hii</h1>
+      <main>
+
+        <div className='text-section'>
+          <h1>Tenzies</h1>
+          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
+
+        <div className='number-section'>
+          <div className='number-row-1'>
+            <Number />
+            <Number />
+            <Number />
+            <Number />
+            <Number />
+          </div>
+          <div className='number-row-2'>
+            <Number />
+            <Number />
+            <Number />
+            <Number />
+            <Number />
+          </div>
+        </div>
+
+        <div className='roll-btn-section'>
+          <button className='roll-btn'>Roll</button>
+        </div>
+
+      </main>
     </div>
   );
 }
---
+++ tenzies-game/src/components/Number.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+export default function Number(props) {
+    return (
+        <button>
+            2
+        </button>
+    )
+}
