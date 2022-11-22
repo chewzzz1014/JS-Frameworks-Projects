76cddf95bd37f9baade84e334187177906b80c34
chewzzz <chewziqing@gmail.com>
2022-11-22 13:37:44 +0800

    Add layout and styles

---
+++ markdown-editor/src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Sidebar(props) {
+    const noteElements = props.notes.map((note, idx) => {
+        <div key={note.id}>
+            <div
+                className={`title ${note.id === props.currentNote.id}? 'selected-note': ''`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className='text-snippet'>Note {idx + 1}</h4>
+            </div>
+        </div>
+    })
+
+    return (
+        <section className='pane sidebar'>
+            <div className='sidebar--header'>
+                <h3>Notes</h3>
+                <button className='new-note' onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    )
+}
