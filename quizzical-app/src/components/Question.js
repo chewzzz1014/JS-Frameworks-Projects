9eb486fb04df9b55dd661605a064fed953deff36
chewzzz <chewziqing@gmail.com>
2022-11-26 20:00:21 +0800

    Add generate new set of questions

--- quizzical-app/src/components/Question.js
+++ quizzical-app/src/components/Question.js
@@ -4,14 +4,24 @@ const he = require('he')
 
 export default function Question() {
 
+    console.log('rerender')
     const [questions, setQuestions] = useState([])
 
     useEffect(() => {
+        getData()
+        console.log('in useEffect')
+    }, [])
+
+    function getData() {
         fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
             .then(data => setQuestions(data.results))
-    }, [])
+    }
 
+    function handleRegenerate() {
+        getData()
+        console.log('in handlerRegenerate')
+    }
 
     const questionElements = questions.map((ele) => {
         const options = [ele.correct_answer, ...ele.incorrect_answers]
@@ -23,13 +33,15 @@ export default function Question() {
         const optionsElements = options.map((o) => {
             if (o === ele.correct_answer)
                 return (
-                    <button>{`${o} (correct answer)`}</button>
+                    <button>{`${he.decode(o)} (correct answer)`}</button>
                 )
             else
                 return (
-                    <button>{o}</button>
+                    <button>{he.decode(o)}</button>
                 )
         })
+
+        console.log('in questionElements')
 
         return (
             <div>
@@ -41,6 +53,7 @@ export default function Question() {
 
     return (
         <div className='question'>
+            <button onClick={handleRegenerate} className='reGen-btn'>Regenerate Questions</button>
             {questionElements}
         </div>
     )
