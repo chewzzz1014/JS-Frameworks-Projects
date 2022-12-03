fe26f269a8c450caa92d68487d894fa5ea02a923
chewzzz <chewziqing@gmail.com>
2022-12-03 16:44:29 +0800

    Add http-module.js

--- Node-Express-Learning/node-tutorial/node-modules/http-module.js
+++ Node-Express-Learning/node-tutorial/node-modules/http-module.js
@@ -1,7 +1,17 @@
-const http = require('htttp')
+const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World')
+    if (req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+    if (req.url === '/about') {
+        res.end('This is about page')
+    }
+    res.end(`
+    <h1>Opps!</h1>
+    <p>Page not found</p>
+    <a href='/'>Back to Home Page</a>
+    `)
 })
 
 server.listen(3000)
