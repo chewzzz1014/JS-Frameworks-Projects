6c06624fd405af56088722ae66b5c320154e3066
chewzzz <chewziqing@gmail.com>
2022-12-04 16:17:01 +0800

    Add emitting event

--- Node-Express-Learning/node-tutorial/node_event/event-emitter.js
+++ Node-Express-Learning/node-tutorial/node_event/event-emitter.js
@@ -4,3 +4,12 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter()
 
+customEmitter.on('response', (name, id) => {
+    console.log(`data received, ${name} (${id})!`)
+})
+customEmitter.on('response', () => {
+    console.log(`some other data`)
+})
+
+customEmitter.emit('response', 'john', 22)
+
