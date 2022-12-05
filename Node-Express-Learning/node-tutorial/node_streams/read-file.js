6a62da43f4ae6e08f336035a321b0225a1f41e52
chewzzz <chewziqing@gmail.com>
2022-12-05 13:46:43 +0800

    Add createReadStream options

--- Node-Express-Learning/node-tutorial/node_streams/read-file.js
+++ Node-Express-Learning/node-tutorial/node_streams/read-file.js
@@ -1,8 +1,16 @@
 const { createReadStream } = require('fs')
 
-const stream = createReadStream('../node-modules/test/first.txt')
+// const stream = createReadStream('../node-modules/test/first.txt')
+const stream = createReadStream('../node-modules/test/first.txt', {
+    highWaterMark: 9000,
+    encoding: 'utf8'
+})
 
 stream.on('data', (result) => {
-    console.log(result) // <Buffer 48 65 6c 6c 6f 20 66 69 72 73 74 21 0d 0a 77 6f 6f 68 6f 6f>
+    console.log(result)
+})
+
+stream.on('error', (err) => {
+    console.log(err)
 })
 
