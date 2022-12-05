5d7fe284822f4928361d8e7e7cf6f99b03ffdafa
chewzzz <chewziqing@gmail.com>
2022-12-05 14:56:56 +0800

    Create basic server

--- Node-Express-Learning/node-express-course/02-express-tutorial/app.js
+++ Node-Express-Learning/node-express-course/02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log('user hit the server')
+
+    // metedata for response
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    })
+
+    res.write('<h1>my home page</h1>')
+    res.end()
+})
+
+server.listen(3000, () => {
+    console.log('Listening at port 3000')
+})
