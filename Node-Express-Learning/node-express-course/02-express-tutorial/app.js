a13aefc0bfb8a90d0a64da73c39cfb0b171821b9
chewzzz <chewziqing@gmail.com>
2022-12-06 18:30:46 +0800

    Add handling different url

--- Node-Express-Learning/node-express-course/02-express-tutorial/app.js
+++ Node-Express-Learning/node-express-course/02-express-tutorial/app.js
@@ -1,15 +1,34 @@
 const http = require('http')
+const { readFileSync } = require('fs')
+
+// get all files
+const homePage = readFileSync('./index.html')
 
 const server = http.createServer((req, res) => {
     console.log(`user hit the server and sent ${req.method} request to ${req.url}`)
 
-    // metedata for response
-    res.writeHead(200, {
-        'content-type': 'text/html'
-    })
+    const url = req.url
 
-    res.write('<h1>my home page</h1>')
-    res.end()
+    if (url === '/') {
+        // metedata for response
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        res.write(homePage)
+        res.end()
+    } else if (url === '/about') {
+        // metedata for response
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        res.write('<h1>about</h1>')
+        res.end()
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>404 Page Not Found</h1>')
+        res.end()
+    }
+
 })
 
 server.listen(3000, () => {
