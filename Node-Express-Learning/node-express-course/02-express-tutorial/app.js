9359ecf9518a5dd734844796c7efef15c57a1278
chewzzz <chewziqing@gmail.com>
2022-12-06 18:09:35 +0800

    Add req method

--- Node-Express-Learning/node-express-course/02-express-tutorial/app.js
+++ Node-Express-Learning/node-express-course/02-express-tutorial/app.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('user hit the server')
+    console.log(`user hit the server and sent ${req.method} request to ${req.url}`)
 
     // metedata for response
     res.writeHead(200, {
