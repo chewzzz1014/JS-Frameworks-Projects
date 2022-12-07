dda295661e6d35334aef4edf9df4a2f40b08946c
chewzzz <chewziqing@gmail.com>
2022-12-07 16:22:29 +0800

    Add GET request for home page

--- Node-Express-Learning/node-express-course/02-express-tutorial/app.js
+++ Node-Express-Learning/node-express-course/02-express-tutorial/app.js
@@ -1,2 +1,21 @@
 const express = require('express')
 const app = express()
+
+// GET
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+// POST
+
+// PUT
+
+// DELETE
+
+// ALL (work with all HTTP Method)
+
+// USE (middleware)
+
+app.listen(3000, () => {
+    console.log('Server running at port 3000')
+})
