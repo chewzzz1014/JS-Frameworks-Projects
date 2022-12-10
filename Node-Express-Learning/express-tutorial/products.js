3887923ab4e572fccca8bcc9ff1fc5536cb58897
chewzzz <chewziqing@gmail.com>
2022-12-10 16:13:24 +0800

    Rename from json.js to products.js

--- Node-Express-Learning/express-tutorial/products.js
+++ Node-Express-Learning/express-tutorial/products.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { products, people } = require("./../node-express-course/02-express-tutorial/data")
+const { products, people } = require("../node-express-course/02-express-tutorial/data")
 const app = express()
 
 app.get("/", (req, res) => {
