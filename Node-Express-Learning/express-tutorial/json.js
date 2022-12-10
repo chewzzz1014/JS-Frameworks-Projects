8035a1f0110f7c1c558755cf8d3e504add3f3b91
chewzzz <chewziqing@gmail.com>
2022-12-10 14:53:41 +0800

    Add data

--- Node-Express-Learning/express-tutorial/json.js
+++ Node-Express-Learning/express-tutorial/json.js
@@ -1,11 +1,9 @@
 const express = require('express')
+const { products, people } = require("./../node-express-course/02-express-tutorial/data")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.json([
-        { name: 'chewzzz' },
-        { name: "John" }
-    ])
+    res.json(products)
 })
 
 app.listen(3000, () => {
