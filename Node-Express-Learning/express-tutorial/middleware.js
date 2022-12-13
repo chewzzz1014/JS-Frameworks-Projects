fc97109aa9af0779bde0ce5a0a977a2b509b00f2
chewzzz <chewziqing@gmail.com>
2022-12-13 18:35:01 +0800

    Add app.use

--- Node-Express-Learning/express-tutorial/middleware.js
+++ Node-Express-Learning/express-tutorial/middleware.js
@@ -10,12 +10,22 @@ const logger = (req, res, next) => {
     next()
 }
 
-app.get("/", logger, (req, res) => {
+app.use("/api", logger)
+
+app.get("/", (req, res) => {
     res.send('Home')
 })
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
     res.send('About')
+})
+
+app.get("/api/products", (req, res) => {
+    res.send('Products')
+})
+
+app.get("/api/items", (req, res) => {
+    res.send('Items')
 })
 
 app.listen(3000, () => {
