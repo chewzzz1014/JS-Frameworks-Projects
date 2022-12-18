55cac1d6405519212359aaf326c4407ecc008325
chewzzz <chewziqing@gmail.com>
2022-12-18 22:41:40 +0800

    Add input fields validation

--- node-passport-authentication/routes/users.js
+++ node-passport-authentication/routes/users.js
@@ -13,4 +13,32 @@ router.get('/register', (req, res) => {
     res.render('register')
 })
 
+router.post('/register', (req, res, next) => {
+    const { name, email, password, password2 } = req.body
+    let errors = []
+
+    // any field is empty?
+    if (!name || !email || !password || !password2) {
+        errors.push({
+            msg: 'Please fill in all fields'
+        })
+    }
+
+    // passwords match?
+    if (password !== password2) {
+        errors.push({
+            msg: 'Passwords do not match'
+        })
+    }
+
+    // password length > 6?
+    if (password.length < 6) {
+        errors.push({
+            msg: 'Password should be at least 6 characters long.'
+        })
+    }
+
+    res.send('hello')
+})
+
 module.exports = router
