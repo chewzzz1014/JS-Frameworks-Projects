8cb69202ae15584eed75fe4e394ed07a0b7b6d98
chewzzz <chewziqing@gmail.com>
2022-12-18 21:16:50 +0800

    Add user and main routes

--- node-passport-authentication/app.js
+++ node-passport-authentication/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const mainRoute = require('./routes/index')
+const userRoute = require('./routes/users')
 
 require('dotenv').config()
 
@@ -9,9 +11,9 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res, next) => {
-    res.send('Hello')
-})
+app.use('/', mainRoute)
+app.use('/user', userRoute)
+
 
 app.listen(process.env.PORT, () => {
     console.log('Server Running')
