fa4f54420cdfd005ce29c4b854d930788081bbcf
chewzzz <chewziqing@gmail.com>
2022-12-20 19:07:17 +0800

    Add convert function to arrow function

--- node-passport-authentication/config/passport.js
+++ node-passport-authentication/config/passport.js
@@ -30,12 +30,12 @@ module.exports = function (passport) {
         console.log(err)
     }
 
-    passport.serializeUser(function (user, done) {
+    passport.serializeUser((user, done) => {
         done(null, user.id)
     })
 
-    passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
             done(err, user)
         })
     })
