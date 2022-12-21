2a1f351165771d487243e27064544164c2b92e15
chewzzz <chewziqing@gmail.com>
2022-12-21 16:28:31 +0800

    Add components; style header

--- expense-tracker/src/components/Header.js
+++ expense-tracker/src/components/Header.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
-            Header
+        <div className='header'>
+            <h2 className='title' styles={{ textAlign: 'center' }}>Expense Tracker</h2>
+            <h4>YOUR BALANCE</h4>
+            <h1>$260.00</h1>
+            <div className='income-expense-container'>
+                <div className='income-expense-box'>
+                    <h4 className='h'>INCOME</h4>
+                    <h4 className='h'>$500.00</h4>
+                </div>
+                <div className='income-expense-box'>
+                    <h4 className='h'>EXPENSE</h4>
+                    <h4 className='h'>$500.00</h4>
+                </div>
+            </div>
         </div>
     )
 }
