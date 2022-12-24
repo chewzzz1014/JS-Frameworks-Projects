99ae57df0b2352f1065dd7d50cba944376a20de5
chewzzz <chewziqing@gmail.com>
2022-12-24 14:24:13 +0800

    Move balance into a single component

---
+++ expense-tracker/src/components/Balance.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Balance() {
+    return (
+        <h1 className='mb-3 balance'>$<span className='balance'>260.00</span></h1>
+    )
+}
+
+export default Balance
--- expense-tracker/src/components/Header.js
+++ expense-tracker/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Balance from './Balance'
 
 function Header() {
     return (
@@ -6,7 +7,7 @@ function Header() {
             <h2 className='title col-12 mb-5'>Expense Tracker</h2>
             <div className='header-balance-display'>
                 <p className='col-12 slight-bold'>YOUR BALANCE</p>
-                <h1 className='mb-3 balance'>$<span className='balance'>260.00</span></h1>
+                <Balance />
             </div>
             <div className=''>
                 <div className='row'>
