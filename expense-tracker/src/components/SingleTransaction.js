3c9d0b75f40c5e016066611539f662987e20e890
chewzzz <chewziqing@gmail.com>
2022-12-24 22:15:24 +0800

    Make sure + and - sign is always after $

--- expense-tracker/src/components/SingleTransaction.js
+++ expense-tracker/src/components/SingleTransaction.js
@@ -8,7 +8,7 @@ export default function SingleTransaction(props) {
         <div className={`col-12 box-with-shadow item-box my-2 ${(amount > 0) ? 'item-income' : 'item-expense'}`}>
             <span className='item-name'>{text}</span>
             <div className='list-right'>
-                <span className='item-price'>{`${(amount > 0) ? '+' : ''}${amount}`}</span>
+                <span className='item-price'>{`${(amount > 0) ? '+' : '-'}$${(amount > 0) ? amount : String(amount).slice(1)}`}</span>
                 <button className='delete-btn'>x</button>
             </div>
         </div>
