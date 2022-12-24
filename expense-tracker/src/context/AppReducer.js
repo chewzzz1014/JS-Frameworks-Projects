509b8e33e5957c4e35c6d1beda667e3a1b80ffb2
chewzzz <chewziqing@gmail.com>
2022-12-24 22:48:10 +0800

    Add delete functionality

--- expense-tracker/src/context/AppReducer.js
+++ expense-tracker/src/context/AppReducer.js
@@ -1,5 +1,13 @@
+// reducer will changes state
+// cannot modify existing state
+
 const reducer = (state, action) => {
     switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(t => t.id !== action.payload)
+            }
         default:
             return state
     }
