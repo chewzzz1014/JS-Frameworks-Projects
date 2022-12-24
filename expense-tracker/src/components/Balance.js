e5bb20718d433156364b278cbe153701ad2bdc18
chewzzz <chewziqing@gmail.com>
2022-12-24 22:23:37 +0800

    Calculate and display the total balance, income ad expense

--- expense-tracker/src/components/Balance.js
+++ expense-tracker/src/components/Balance.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 function Balance() {
+
+    const { transactions } = useContext(GlobalContext)
+
     return (
         < div className='header-balance-display' >
             <p className='col-12 slight-bold'>YOUR BALANCE</p>
             <h1 className='mb-3 balance'>
                 $
                 <span className='balance'>
-                    260.00
+                    {transactions.reduce((total, t) => total + t.amount, 0)}
                 </span>
             </h1>
         </div>
--- expense-tracker/src/components/IncomeExpense.js
+++ expense-tracker/src/components/IncomeExpense.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 function IncomeExpense() {
+
+    const { transactions } = useContext(GlobalContext)
+
     return (
         <div className=''>
             <div className='row'>
                 <div className='box-with-shadow income-expense-box col-6'>
                     <p className='slight-bold mt-2 larger-text'>INCOME</p>
-                    <p className='income'>$500.00</p>
+                    <p className='income'>${transactions.reduce((total, t) =>
+                        (t.amount > 0) ? total + t.amount : total
+                        , 0)}
+                    </p>
                 </div>
                 <div className='box-with-shadow income-expense-box col-6'>
                     <p className='slight-bold mt-2 larger-text'>EXPENSE</p>
-                    <p className='expense'>$500.00</p>
+                    <p className='expense'>$
+                        {transactions.reduce((total, t) =>
+                            (t.amount < 0) ? total + Math.abs(t.amount) : total
+                            , 0)}
+                    </p>
                 </div>
             </div>
         </div >
