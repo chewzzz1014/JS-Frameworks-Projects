ed73b11fd7250e2b9665211e66e278bcbbbcc7b8
chewzzz <chewziqing@gmail.com>
2022-12-24 14:37:37 +0800

    Add delete button for items in list

--- expense-tracker/src/components/TransactionList.js
+++ expense-tracker/src/components/TransactionList.js
@@ -6,15 +6,24 @@ function TransactionList() {
             <div className='row'>
                 <div className='col-12 box-with-shadow item-box my-2 item-income'>
                     <span className='item-name'>Cash</span>
-                    <span className='item-price'>+500</span>
+                    <div className='list-right'>
+                        <span className='item-price'>+500</span>
+                        <button class='delete-btn'>x</button>
+                    </div>
                 </div>
                 <div className='col-12 box-with-shadow item-box my-2 item-expense'>
                     <span className='item-name '>Cash</span>
-                    <span className='item-price'>-40</span>
+                    <div className="list-right">
+                        <span className='item-price'>-40</span>
+                        <button class='delete-btn'>x</button>
+                    </div>
                 </div>
                 <div className='col-12 box-with-shadow item-box my-2 item-expense'>
                     <span className='item-name'>Cash</span>
-                    <span className='item-price'>-200</span>
+                    <div className="list-right">
+                        <span className='item-price'>-200</span>
+                        <button class='delete-btn'>x</button>
+                    </div>
                 </div>
             </div>
         </div>
