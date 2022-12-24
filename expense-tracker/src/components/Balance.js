ebf4b87840a99ab95aead1afa5df3afb8a7c9a99
chewzzz <chewziqing@gmail.com>
2022-12-24 14:26:13 +0800

    Make JSX readable

--- expense-tracker/src/components/Balance.js
+++ expense-tracker/src/components/Balance.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Balance() {
     return (
-        <h1 className='mb-3 balance'>$<span className='balance'>260.00</span></h1>
+        <h1 className='mb-3 balance'>
+            $
+            <span className='balance'>
+                260.00
+            </span>
+        </h1>
     )
 }
 
