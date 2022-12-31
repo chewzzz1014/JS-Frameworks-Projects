603fb28db1fc13ec2cd77ea336224bf672b111a9
chewzzz <chewziqing@gmail.com>
2022-12-31 14:29:55 +0800

    Add POST, DELETE AND GET to the router

--- expense-tracker-fullStack/controllers/transaction.js
+++ expense-tracker-fullStack/controllers/transaction.js
@@ -2,11 +2,27 @@
 
 // @desc Get all transactions
 // @route /api/v1/transactions
-// @access Publib
+// @access Public
 const getTransactions = (req, res, next) => {
     res.send('GET transaction')
 }
 
+// @desc Add transaction
+// @route /api/v1/transactions
+// @access Public
+const addTransactions = (req, res, next) => {
+    res.send('POST transaction')
+}
+
+// @desc Delete transaction
+// @route /api/v1/transactions/:id
+// @access Public
+const deleteTransaction = (req, res, next) => {
+    res.send('DELETE transaction')
+}
+
 module.exports = {
-    getTransactions
+    getTransactions,
+    addTransactions,
+    deleteTransaction
 }
--- expense-tracker-fullStack/routes/transactions.js
+++ expense-tracker-fullStack/routes/transactions.js
@@ -1,6 +1,13 @@
 const router = require('express').Router()
-const { getTransactions } = require('../controllers/transaction')
+const { getTransactions, addTransactions, deleteTransaction } = require('../controllers/transaction')
 
-router.route('/').get(getTransactions)
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransactions)
+
+router
+    .route('/:id')
+    .delete(deleteTransaction)
 
 module.exports = router
