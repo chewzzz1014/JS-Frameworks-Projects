02686007e094327272caf7e4eae7be62c27f4867
chewzzz <chewziqing@gmail.com>
2022-12-31 22:24:10 +0800

    Rename html document name and add landing page description

--- quizzical-app/src/components/Landing.js
+++ quizzical-app/src/components/Landing.js
@@ -7,7 +7,7 @@ export default function Landing(props) {
     return (
         <div className='landing'>
             <h1 className='landing-txt'>Quizzical</h1>
-            <p className='landing-txt'>Some description if needed</p>
+            <p className='landing-txt'>Test Your Knowlege on Different Domains!</p>
             <button onClick={handler} className='utils-btn start-btn'>Start Quiz</button>
         </div>
     )
