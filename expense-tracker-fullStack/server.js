a1586035d10f26bcf3a2be75381643dcb251593d
chewzzz <chewziqing@gmail.com>
2022-12-31 13:50:42 +0800

    Add basic GET request handler

---
+++ expense-tracker-fullStack/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const colors = require('colors')
+const app = express()
+
+require('dotenv').config({ path: './config/config.js' })
+app.use(morgan())
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT} (${process.env.NODE_ENV} mode)`.yellow.bold)
+})
