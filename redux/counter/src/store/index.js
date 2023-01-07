fe664305404577769516ed9b1152e8a5dce79918
chewzzz <chewziqing@gmail.com>
2023-01-07 13:48:44 +0800

    Create store

---
+++ redux/counter/src/store/index.js
@@ -0,0 +1,10 @@
+// put all React state here
+import { legacy_createStore } from 'redux'
+
+// reducer
+// assign initial state 
+const reducerFunc = (state = { counter: 0 }, action) => {
+
+}
+
+const store = legacy_createStore(reducerFunc)
