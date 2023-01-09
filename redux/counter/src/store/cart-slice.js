e418d7c642376dc06e7598f38766aaa1620ae9d7
chewzzz <chewziqing@gmail.com>
2023-01-09 15:12:13 +0800

    Add cart slice

---
+++ redux/counter/src/store/cart-slice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        itemList: [],
+        totalQuantity: 0,
+        showCart: false
+    },
+    reducers: {
+        addToCart(state, action) {
+            const newItem = action.payload
+
+            const existingItem = state.itemList.find(i => i.id === newItem.id)
+
+            if (existingItem) {
+                existingItem.quantity++
+                existingItem.price += newItem.price
+            } else {
+                state.itemList.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.name
+                })
+            }
+        },
+        removeFromCart() { },
+        setShowCart(state) {
+            state.showCart = true
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions
+
+export default cartSlice
