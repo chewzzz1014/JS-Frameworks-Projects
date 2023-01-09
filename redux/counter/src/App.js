7d65682b8c792f54ac82706ef8772e60273149dc
chewzzz <chewziqing@gmail.com>
2023-01-09 14:20:20 +0800

    Add start code of the course

--- redux/counter/src/App.js
+++ redux/counter/src/App.js
@@ -1,42 +1,13 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { actions } from './store/index'
+import React from "react";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
 
 function App() {
-  const counter = useSelector((state) => state.counter)
-  const dispatch = useDispatch()
-
-  // dispatch action
-  const increment = () => {
-    // dispatch({
-    //   type: 'INC',
-    // })
-    dispatch(actions.increment())
-  }
-
-  // dispatch action
-  const decrement = () => {
-    // dispatch({
-    //   type: 'DESC'
-    // })
-    dispatch(actions.decrement())
-  }
-
-  const addBy = () => {
-    // dispatch({
-    //   type: 'ADD',
-    //   payload: 10
-    // })
-    dispatch(actions.addBy(10))
-  }
-
   return (
     <div className="App">
-      <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={increment}>Increase</button>
-      <button onClick={decrement}>Decrease</button>
-      <button onClick={addBy}>Add By 10</button>
+      <Auth />
+      {/* <Layout /> */}
     </div>
   );
 }
