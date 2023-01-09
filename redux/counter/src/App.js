378ec1657945de794e57a9b48506da8cf7dcdc1a
chewzzz <chewziqing@gmail.com>
2023-01-09 14:47:36 +0800

    Add conditional rendering of login and main page

--- redux/counter/src/App.js
+++ redux/counter/src/App.js
@@ -9,8 +9,7 @@ function App() {
   console.log(isLoggedIn)
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {isLoggedIn ? <Layout /> : <Auth />}
     </div>
   );
 }
