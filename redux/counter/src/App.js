3847e8cb57c86a6746ed964e5b289bcd31074399
chewzzz <chewziqing@gmail.com>
2023-01-13 16:35:37 +0800

    Add firebase real time database

--- redux/counter/src/App.js
+++ redux/counter/src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { useSelector } from 'react-redux'
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector(state => state.cart)
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
-  console.log(isLoggedIn)
 
-  const cartItems = useSelector((state) => state.cart.itemList)
-  console.log(cartItems)
+  useEffect(() => {
+
+    const sendRequest = async () => {
+      const res = await fetch('https://redux-tutorial-http-18870-default-rtdb.asia-southeast1.firebasedatabase.app/cartItems.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+      const data = await res.json()
+    }
+
+    sendRequest()
+  }, [cart])
 
   return (
     <div className="App">
