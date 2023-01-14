e68959e08f238e299aa62a95b95c84a5d014ddf1
chewzzz <chewziqing@gmail.com>
2023-01-14 22:19:33 +0800

    Add 9 squares

--- react-tutorial/src/components/Square.js
+++ react-tutorial/src/components/Square.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Square() {
+export default function Square({ value }) {
     return (
         <button className="square">
-            X
+            {value}
         </button>
     )
 }
