00eef912b253278650f16cb5f21e392727264d32
chewzzz <chewziqing@gmail.com>
2023-01-15 18:22:07 +0800

    Complete Tic Tac Toe game with history of movement

--- react-tutorial/src/components/Game.js
+++ react-tutorial/src/components/Game.js
@@ -3,11 +3,12 @@ import Board from "./Board";
 
 export default function Game() {
 
-    const [xIsNext, setXIsNext] = useState(true)
     const [history, setHistory] = useState([Array(9).fill(null)])
+    const [currentMove, setCurrentMove] = useState(0)
+    const xIsNext = currentMove % 2 === 0
 
     // last history
-    const currentSquares = history[history.length - 1]
+    const currentSquares = history[currentMove]
 
     // render a list of moves
     const moves = history.map((squares, move) => {
@@ -24,12 +25,13 @@ export default function Game() {
     })
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares])
-        setXIsNext(!xIsNext)
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory)
+        setCurrentMove(nextHistory.length - 1)
     }
 
     function jumpTo(nextMove) {
-
+        setCurrentMove(nextMove)
     }
 
     return (
