56d6db1abdb27def447a54bd907a4fe7749037d7
chewzzz <chewziqing@gmail.com>
2023-01-16 15:55:30 +0800

    Add field prop in root query

--- graphql-server/schema.js
+++ graphql-server/schema.js
@@ -29,11 +29,19 @@ const CustomerType = new GraphQLObjectType({
 // root query
 const rootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    customer: {
-        type: CustomerType
-    }
+    field: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                customers.filter(c => c.id === args.id)
+            }
+        }
+    },
 })
 
 module.exports = new GraphQLSchema({
-
+    query: rootQuery
 })
