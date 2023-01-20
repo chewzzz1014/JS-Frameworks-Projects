8f56001c6023103b2dd268c4cca34eade3194708
chewzzz <chewziqing@gmail.com>
2023-01-20 12:31:35 +0800

    Add logger and error handler

--- Exercise-Tracker/app.js
+++ Exercise-Tracker/app.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }))
 // to be able to read .env file
@@ -35,5 +39,21 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log("Listening to Port 3000")
 })
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // Send the error status
+    res.status(err.status || 500);
+    res.send(err.message);
+});
 
 export default app
