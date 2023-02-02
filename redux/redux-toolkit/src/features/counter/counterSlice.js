6b8c4c6a5739d8284a4c344bcdf43c9c4ef712a9
chewzzz <chewziqing@gmail.com>
2023-02-02 17:37:30 +0800

    Create counterSlice

---
+++ redux/redux-toolkit/src/features/counter/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0
+}
+
+// counter slice 
+export const counterSlice = createSlice({
+    name: 'counter',
+    // data
+    initialState,
+    // actions
+    reducers: {
+
+        increment: (state) => {
+            state.count += 1
+        },
+
+        decrement: (state) => {
+            state.count -= 1
+        }
+    }
+})
+
+export const { increment, decrement } = counterSlice.actions
