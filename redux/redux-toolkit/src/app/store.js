022683c7f3109455a2582c20392367aa44cd274b
chewzzz <chewziqing@gmail.com>
2023-02-02 17:44:28 +0800

    Add counter reducer to store

--- redux/redux-toolkit/src/app/store.js
+++ redux/redux-toolkit/src/app/store.js
@@ -1,8 +1,9 @@
 // contains all stores of the project
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
     reducer: {
-
+        counter: counterReducer,
     }
 })
--- redux/redux-toolkit/src/features/counter/counterSlice.js
+++ redux/redux-toolkit/src/features/counter/counterSlice.js
@@ -23,3 +23,6 @@ export const counterSlice = createSlice({
 })
 
 export const { increment, decrement } = counterSlice.actions
+
+// export for the store
+export default counterSlice.reducer
