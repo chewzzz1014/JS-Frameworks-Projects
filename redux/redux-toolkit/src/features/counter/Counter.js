df899c997ed2be7c9d7afd129b199acb1b577829
chewzzz <chewziqing@gmail.com>
2023-02-03 16:38:23 +0800

    Add increment by amount textfield and button

--- redux/redux-toolkit/src/features/counter/Counter.js
+++ redux/redux-toolkit/src/features/counter/Counter.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './counterSlice'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice'
+import { useState } from 'react'
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count)
     const dispatch = useDispatch()
+
+    const [incrementAmount, setIncrementAmount] = useState(0)
+    // make sure that the state is always numeric
+    const addValue = Number(incrementAmount) || 0
+
+    const resetAll = () => {
+        setIncrementAmount(0)
+        dispatch(reset())
+    }
 
     return (
         <section>
@@ -14,6 +24,16 @@ const Counter = () => {
             <div>
                 <button onClick={() => dispatch(increment())}>+</button>
                 <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+
+            <input
+                type="text"
+                value={incrementAmount}
+                onChange={(e) => setIncrementAmount(e.target.value)}
+            />
+
+            <div>
+                <button>Add Amount</button>
             </div>
         </section>
     )
