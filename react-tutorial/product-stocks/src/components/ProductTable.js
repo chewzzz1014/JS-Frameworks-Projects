a09a62e86184fe21710653f3bdb23b290be1cd22
chewzzz <chewziqing@gmail.com>
2023-02-06 14:19:10 +0800

    Assign array to a variabShow products that match the keyword and inStock state

--- react-tutorial/product-stocks/src/components/ProductTable.js
+++ react-tutorial/product-stocks/src/components/ProductTable.js
@@ -1,11 +1,19 @@
 import ProductCategoryRow from './ProductCategoryRow'
 import ProductRow from './ProductRow'
 
-export default function ProductTable({ products }) {
+export default function ProductTable({ products, filterText, inStoackOnly }) {
     const rows = []
     let categories = []
 
     products.forEach(p => {
+
+        // did not match search keyword
+        if (p.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1)
+            return
+
+        // did user require in stock data only?
+        if (inStoackOnly && !p.stocked)
+            return
 
         // create a category header
         if (!categories.includes(p.category)) {
