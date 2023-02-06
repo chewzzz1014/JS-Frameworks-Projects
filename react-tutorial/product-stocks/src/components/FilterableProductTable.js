1ff44713adb3a31ae7a9df880a239522c6142613
chewzzz <chewziqing@gmail.com>
2023-02-06 14:10:02 +0800

    Add states in parent component

--- react-tutorial/product-stocks/src/components/FilterableProductTable.js
+++ react-tutorial/product-stocks/src/components/FilterableProductTable.js
@@ -1,7 +1,11 @@
 import SearchBar from './SearchBar'
 import ProductTable from './ProductTable'
+import { useState } from 'react'
 
 export default function FilterableProductTable() {
+    const [filterText, setFilterText] = useState('')
+    const [inStockOnly, setInStockOnly] = useState(false)
+
     return (
         <div>
             <SearchBar />
