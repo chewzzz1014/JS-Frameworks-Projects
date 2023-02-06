f7de7717232a434d45b7a66bc2a4c9af549a6a3b
chewzzz <chewziqing@gmail.com>
2023-02-06 14:21:03 +0800

    Add states to SearchBar component

--- react-tutorial/product-stocks/src/components/SearchBar.js
+++ react-tutorial/product-stocks/src/components/SearchBar.js
@@ -1,12 +1,19 @@
-export default function SearchBar() {
+export default function SearchBar({ filterText, inStockOnly }) {
     return (
         <form>
             <div>
-                <input type="text" placeholder="Search..." />
+                <input
+                    type="text"
+                    value={filterText}
+                    placeholder="Search..."
+                />
             </div>
             <div>
                 <label>
-                    <input type="checkbox" />
+                    <input
+                        type="checkbox"
+                        checked={inStockOnly}
+                    />
                     {' Only show products in stock'}
                 </label>
             </div>
