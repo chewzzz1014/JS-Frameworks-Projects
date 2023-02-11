5fd1a4d8d45ce9621b4c51847a0a6871df0c0dae
chewzzz <chewziqing@gmail.com>
2023-02-11 20:03:22 +0800

    Change tab icon based on state

--- markdown-previewer/src/components/TabHeading.js
+++ markdown-previewer/src/components/TabHeading.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { FaExpandArrowsAlt, FaExpandAlt } from 'react-icons/fa';
 
 function TabHeading({ text, isFull, setIsFull }) {
+
     return (
         <div className='bg-slate-400 flex justify-between items-center px-3 py-1'>
             <div><b>{text}</b></div>
-            {!isFull ? <FaExpandArrowsAlt /> : <FaExpandAlt />}
+            <button
+                onClick={() => setIsFull(!isFull)}
+            >
+                {!isFull ? <FaExpandArrowsAlt /> : <FaExpandAlt />}
+            </button>
         </div>
     )
 }
