8547d100f53ad7d9bb2a4c3bd2b73bce3d9bd9f8
chewzzz <chewziqing@gmail.com>
2023-02-11 19:11:34 +0800

    Make markdown previewer div align-auto

--- markdown-previewer/src/components/Previewer.js
+++ markdown-previewer/src/components/Previewer.js
@@ -4,11 +4,13 @@ import Markdown from 'marked-react'
 function Previewer({ text }) {
     return (
         <div
-            className='border-2 border-red-600 prose lg:prose-x'
+            className='border-2 border-red-600 prose lg:prose-xl w-11/12 mx-auto md:w-4/5'
         >
-            <Markdown>
-                {text}
-            </Markdown>
+            <div className='p-5'>
+                <Markdown>
+                    {text}
+                </Markdown>
+            </div>
         </div>
     )
 }
