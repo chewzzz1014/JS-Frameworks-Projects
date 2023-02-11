46814aa0af8410f08d0c87823de90aee910b16a1
chewzzz <chewziqing@gmail.com>
2023-02-11 19:17:05 +0800

    Align editor component to center; add margin top

--- markdown-previewer/src/components/Editor.js
+++ markdown-previewer/src/components/Editor.js
@@ -3,11 +3,10 @@ import React from 'react'
 function Editor({ text, handleTextChange }) {
     return (
         <div
-            className='border-2 border-yellow-500'
+            className='border-2 border-yellow-500 w-4/5 mx-auto md:w-1/3 mt-5'
         >
             <textarea
                 id='editor'
-                cols='100'
                 className='border-2 border-purple-600'
                 onChange={(e) => handleTextChange(e)}
             >
--- markdown-previewer/src/components/Previewer.js
+++ markdown-previewer/src/components/Previewer.js
@@ -4,7 +4,7 @@ import Markdown from 'marked-react'
 function Previewer({ text }) {
     return (
         <div
-            className='border-2 border-red-600 prose lg:prose-xl w-11/12 mx-auto md:w-4/5'
+            className='border-2 border-red-600 prose lg:prose-xl w-11/12 mx-auto md:w-4/5 mt-5'
         >
             <div className='p-5'>
                 <Markdown>
