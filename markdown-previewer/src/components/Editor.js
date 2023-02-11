bb7a8c27c8036036d1f45aa3c96a633b477794b2
chewzzz <chewziqing@gmail.com>
2023-02-11 19:30:20 +0800

    Adjust width of editor at md breakpoint

--- markdown-previewer/src/components/Editor.js
+++ markdown-previewer/src/components/Editor.js
@@ -3,11 +3,12 @@ import React from 'react'
 function Editor({ text, handleTextChange }) {
     return (
         <div
-            className='border-2 border-yellow-500 w-4/5 mx-auto md:w-1/3 mt-5'
+            className='border-2 border-yellow-500 w-4/5 mx-auto md:w-4/12 mt-5'
         >
             <textarea
                 id='editor'
-                className='border-2 border-purple-600'
+                rows='15'
+                className='border-2 border-purple-600 w-full'
                 onChange={(e) => handleTextChange(e)}
             >
                 {text}
