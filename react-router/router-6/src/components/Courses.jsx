b3139a694bca2dc9c4319a57f3321e31291a5b4b
chewzzz <chewziqing@gmail.com>
2023-03-13 20:07:51 +0800

    Add courseList; add NavLink

--- react-router/router-6/src/components/Courses.jsx
+++ react-router/router-6/src/components/Courses.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 function Courses() {
+    const courseList = ['React', 'Angular', 'Vue', 'Nodejs']
+    const randomCourseName = courseList[Math.floor(Math.random() * courseList.length)]
+
     return (
         <div>
             <h1>Course List</h1>
             <h4>Course card</h4>
+
+            <p>More test</p>
+            <NavLink></NavLink>
             <Outlet />
         </div>
     )
