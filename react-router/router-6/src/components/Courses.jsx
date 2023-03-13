fa94cb0067cbad066d42c06f1e04d556b8f1190b
chewzzz <chewziqing@gmail.com>
2023-03-13 19:21:44 +0800

    Add Bundles and Courses component

---
+++ react-router/router-6/src/components/Courses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Courses() {
+    return (
+        <div>
+            <h1>Course List</h1>
+            <h1>Course List</h1>
+            <h4>Course card</h4>
+        </div>
+    )
+}
+
+export default Courses
