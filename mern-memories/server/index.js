c27c5228c783918a6db1926c53362a3bab21ec8d
chewzzz <chewziqing@gmail.com>
2023-03-14 20:37:39 +0800

    Install and use dotenv

--- mern-memories/server/index.js
+++ mern-memories/server/index.js
@@ -4,9 +4,11 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRouter from './routes/posts.js'
+import dotenv from 'dotenv'
 
 const app = express()
 
+dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
