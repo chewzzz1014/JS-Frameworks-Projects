321484d6d43d18af9c0a4ea20548bb164ef1e5c3
chewzzz <chewziqing@gmail.com>
2023-03-15 09:14:22 +0800

    apply styles

--- mern-memories/client/src/App.jsx
+++ mern-memories/client/src/App.jsx
@@ -9,15 +9,18 @@ import {
 import memories from './images/memories.png'
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
+import useStyles from './styles.js'
 
 function App() {
+    const classes = useStyles()
+
     return (
         <Container maxWidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>
                     Memories
                 </Typography>
-                <img src={memories} alt='memories' height='60' />
+                <img className={classes.image} src={memories} alt='memories' height='60' />
             </AppBar>
             <Grow in>
                 <Container>
