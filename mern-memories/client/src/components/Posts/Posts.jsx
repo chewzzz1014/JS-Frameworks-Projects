e58d5fba10354a8d52159a13730b18c8ac48ca43
chewzzz <chewziqing@gmail.com>
2023-03-15 09:11:20 +0800

    Add styles for App using material makeStyles

--- mern-memories/client/src/components/Posts/Posts.jsx
+++ mern-memories/client/src/components/Posts/Posts.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Post from './Post/Post'
 
 function Posts() {
     return (
         <div>
-
+            posts
+            <Post />
+            <Post />
         </div>
     )
 }
