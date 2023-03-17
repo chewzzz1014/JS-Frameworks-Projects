99430b49b0537aa1bbf9f0f9dd8c3a8c51742446
chewzzz <chewziqing@gmail.com>
2023-03-17 22:06:26 +0800

    Return result of promise

--- mern-memories/client/src/actions/posts.js
+++ mern-memories/client/src/actions/posts.js
@@ -3,7 +3,9 @@ import * as api from '../api'
 // action creators
 const getPosts = () => async (dispatch) => {
     try {
-        const { data } = await api.fetchPosts()
+        const {data} = await api.fetchPosts()
+        console.log(data)
+
         dispatch({
             type: 'FETCH_ALL',
             payload: data
@@ -15,7 +17,8 @@ const getPosts = () => async (dispatch) => {
 
 const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await api.createPost(post)
+        const {data} = await api.createPost(post)
+        console.log(data)
 
         dispatch({
             type: 'CREATE',
--- mern-memories/client/src/api/index.js
+++ mern-memories/client/src/api/index.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:5000/posts'
 
 const fetchPosts = async () => {
-    await axios.get(BASE_URL)
+    return await axios.get(BASE_URL)
 }
 
 const createPost = async (newPost) => {
-    await axios.post(BASE_URL, newPost)
+    return await axios.post(BASE_URL, newPost)
 }
 
 export {
