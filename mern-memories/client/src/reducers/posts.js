737e902b02c97e5847c83d14bd4bb18420352ed5
chewzzz <chewziqing@gmail.com>
2023-03-18 13:25:30 +0800

    Add updatePost reducer

--- mern-memories/client/src/reducers/posts.js
+++ mern-memories/client/src/reducers/posts.js
@@ -8,6 +8,10 @@ const reducer = (posts = [], action) => {
                 ...posts,
                 action.payload
             ]
+        case 'UPDATE':
+            return posts.map(p =>
+                p._id === action.payload._id ? action.payload : p
+            )
         default:
             return posts
     }
