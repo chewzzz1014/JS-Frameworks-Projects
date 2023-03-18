5098b8797e63f03dddd054b0fe03b8f0f1e4d827
chewzzz <chewziqing@gmail.com>
2023-03-18 17:26:54 +0800

    Add Auth component

--- mern-memories/client/src/App.jsx
+++ mern-memories/client/src/App.jsx
@@ -7,15 +7,20 @@ import {
 } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
+import Auth from './components/Auth/Auth'
 
 function App() {
-
-
     return (
-        <Container maxWidth='lg'>
-            <NavBar />
-            <Hone />
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth='lg'>
+                <NavBar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/auth' exact component={Auth} />
+                </Switch>
+                <Home />
+            </Container>
+        </BrowserRouter>
     )
 }
 
---
+++ mern-memories/client/src/components/Auth/Auth.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Auth() {
+    const state = null
+
+    return (
+        <div>
+            Auth
+        </div>
+    )
+}
+
+export default Auth
