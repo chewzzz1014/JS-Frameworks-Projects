a8760d10f22cbc98900822611903497d605e14ec
chewzzz <chewziqing@gmail.com>
2023-03-18 17:21:26 +0800

    Move main logic and main page into Home component

--- mern-memories/client/src/App.jsx
+++ mern-memories/client/src/App.jsx
@@ -1,41 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { Container } from '@material-ui/core'
 import {
-    Container,
-    AppBar,
-    Typography,
-    Grow,
-    Grid
-} from '@material-ui/core'
-import { useDispatch } from 'react-redux'
-
-import { getPosts } from './actions/posts'
-import Form from './components/Form/Form'
-import Posts from './components/Posts/Posts'
-import useStyles from './styles.js'
+    BrowserRouter,
+    Switch,
+    Route
+} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
 
 function App() {
-    const [currentId, setCurrentId] = useState(null)
-    const classes = useStyles()
-    const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [dispatch, currentId])
 
     return (
         <Container maxWidth='lg'>
-            <Grow in>
-                <Container>
-                    <Grid container justify-content='space-between' alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+            <NavBar />
+            <Hone />
         </Container>
     )
 }
