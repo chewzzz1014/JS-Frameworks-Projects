06bbc84bb79862f4d144f6591035f2d4231922d6
chewzzz <chewziqing@gmail.com>
2023-03-22 14:09:24 +0800

    Add body of Auth component

--- mern-memories/client/src/components/Auth/Auth.jsx
+++ mern-memories/client/src/components/Auth/Auth.jsx
@@ -5,17 +5,45 @@ import {
     Paper,
     Grid,
     Typography,
-    Container
+    Container,
+    TextField
 } from '@material-ui/core'
 import useStyles from './styles'
+import LockOutlinedIcon, { Looks } from '@material-ui/icons/LockOutlined'
 
 function Auth() {
-    const state = null
+    const classes = useStyles()
+    const isSignUp = false
+    const handleSubmit = () => {
+
+    }
+    const handleChange = () => {
+
+    }
 
     return (
-        <div>
-            Auth
-        </div>
+        <Container component='main' maxWidth='xs'>
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant='h5'>
+                    {isSignUp ? 'Sign Up' : 'Sign In'}
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignUp && {
+                                <>
+                            <TextField name='firstName' label='First Name' handleChange={handleChange} autoFocus xs={6}>
+                            </TextField>
+                                </>
+                            }
+                        }
+                </Grid>
+            </form>
+        </Paper>
+        </Container >
     )
 }
 
