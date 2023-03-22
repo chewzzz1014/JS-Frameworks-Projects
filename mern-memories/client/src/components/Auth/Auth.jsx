caa2e2a8bc294fa29c87d823de966627be585393
chewzzz <chewziqing@gmail.com>
2023-03-22 17:03:23 +0800

    Add GoogleOAuthProvider

--- mern-memories/client/src/components/Auth/Auth.jsx
+++ mern-memories/client/src/components/Auth/Auth.jsx
@@ -11,6 +11,7 @@ import {
 import useStyles from './styles'
 import LockOutlinedIcon, { Looks } from '@material-ui/icons/LockOutlined'
 import Input from './Input'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function Auth() {
     const classes = useStyles()
@@ -29,64 +30,66 @@ function Auth() {
     const switchMode = () => setIsSignUp((prev) => !prev)
 
     return (
-        <Container component='main' maxWidth='xs'>
-            <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography variant='h5'>
-                    {isSignUp ? 'Sign Up' : 'Sign In'}
-                </Typography>
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
-                        {
-                            isSignUp && (
-                                <>
-                                    <Input
-                                        name='firstName'
-                                        label='First Name'
-                                        handleChange={handleChange}
-                                        autoFocus
-                                        half
-                                    />
-                                    <Input
-                                        name='firstName'
-                                        label='First Name'
-                                        handleChange={handleChange}
-                                        half
-                                    />
-                                </>
-                            )
-                        }
-                        <Input
-                            name='email'
-                            label='Email Address'
-                            handleChange={handleChange}
-                            type='email'
-                        />
-                        <Input
-                            name='password'
-                            label='Password'
-                            handleChange={handleChange}
-                            type={showPassword ? 'text' : 'password'}
-                            handleShowPassword={handleShowPassword}
-                        />
-                        {isSignUp && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' />}
-                    </Grid>
-
-                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+        <GoogleOAuthProvider clientId=''>
+            <Container component='main' maxWidth='xs'>
+                <Paper className={classes.paper} elevation={3}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography variant='h5'>
                         {isSignUp ? 'Sign Up' : 'Sign In'}
-                    </Button>
-                    <Grid container justifyContent='flex-end'>
-                        <Grid item>
-                            <Button onClick={switchMode}>
-                                {isSignUp ? 'Already have an account? Sign In' : "Dont's have an account? Sign Up"}
-                            </Button>
+                    </Typography>
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        <Grid container spacing={2}>
+                            {
+                                isSignUp && (
+                                    <>
+                                        <Input
+                                            name='firstName'
+                                            label='First Name'
+                                            handleChange={handleChange}
+                                            autoFocus
+                                            half
+                                        />
+                                        <Input
+                                            name='firstName'
+                                            label='First Name'
+                                            handleChange={handleChange}
+                                            half
+                                        />
+                                    </>
+                                )
+                            }
+                            <Input
+                                name='email'
+                                label='Email Address'
+                                handleChange={handleChange}
+                                type='email'
+                            />
+                            <Input
+                                name='password'
+                                label='Password'
+                                handleChange={handleChange}
+                                type={showPassword ? 'text' : 'password'}
+                                handleShowPassword={handleShowPassword}
+                            />
+                            {isSignUp && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' />}
                         </Grid>
-                    </Grid>
-                </form>
-            </Paper>
-        </Container >
+
+                        <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                            {isSignUp ? 'Sign Up' : 'Sign In'}
+                        </Button>
+                        <Grid container justifyContent='flex-end'>
+                            <Grid item>
+                                <Button onClick={switchMode}>
+                                    {isSignUp ? 'Already have an account? Sign In' : "Dont's have an account? Sign Up"}
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+            </Container >
+        </GoogleOAuthProvider>
     )
 }
 
