1e3a2fba019afa91a87231a21d0313b9b71de1d5
chewzzz <chewziqing@gmail.com>
2023-10-18 21:14:29 +0800

    first vue project

---
+++ vue-basics/modal-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      title: "My First Vue App",
+    };
+  },
+  methods: {
+    handleClick(e) {
+      // access template ref
+      console.log(this.$refs.name)
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
