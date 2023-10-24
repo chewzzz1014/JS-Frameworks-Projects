6cd6e40bab7c30f9dc7e62b4d518b7559218a523
chewzzz <chewziqing@gmail.com>
2023-10-24 23:40:14 +0800

    add styles

--- vue-basics/modal-project/src/components/Modal.vue
+++ vue-basics/modal-project/src/components/Modal.vue
@@ -2,7 +2,7 @@
     <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{ sale: theme === 'sale' }">
             <slot>default slot content</slot>
-            <div class="action">
+            <div class="actions">
                 <slot name="links"></slot>
             </div>
         </div>
@@ -41,6 +41,27 @@
         width: 100%;
         height: 100%;
     }
+    .modal h1 {
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+
+    .modal .actions {
+        text-align: center;
+        margin: 30px 0 10px 0;
+        color: #333;
+    }
+
+    .modal .actions a {
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
+
     h1 {
         color: rgb(157, 241, 12);
     }
